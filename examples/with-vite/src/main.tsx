a7f38b8be1d3914b08d3cd0626487eae5d5dfc22
wbh1328551759 <1328551759@qq.com>
2025-05-31 21:05:47 +0800

    feat: update with-vite of config

--- examples/with-vite/src/main.tsx
+++ examples/with-vite/src/main.tsx
@@ -2,31 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {createConfig, kusama, LunoKitProvider, polkadot, polkadotjs, subwallet, wsProvider} from '@luno-kit/ui'
+import {createConfig, kusama, LunoKitProvider, polkadot, polkadotjs, subwallet, wsProvider, westend, paseo} from '@luno-kit/ui'
 import '@luno-kit/ui/dist/styles.css'
 
 const connectors = [
   polkadotjs(), subwallet()
 ]
 
-const gebSignet = {
-  genesisHash: '0xe676fe35e3e2a37fb911ab4ec0857870f1cdc10b04a7b2f0f64eb31e9a142067',
-  name: 'geb-signet',
-  nativeCurrency: { name: 'GEB', symbol: 'GEB', decimals: 8 },
-  rpcUrls: { webSocket: ['wss://signet.geb.network/ws'] },
-  ss58Format: 44,
-  chainIconUrl: ''
-};
-
 const lunoConfig = createConfig({
   appName: 'luno with-vite example',
-  chains: [polkadot, kusama, gebSignet],
+  chains: [polkadot, kusama, westend, paseo],
   connectors: connectors,
   autoConnect: true,
   transports: {
     [polkadot.genesisHash]: wsProvider('wss://polkadot.api.onfinality.io/public-ws'),
     [kusama.genesisHash]: wsProvider('wss://kusama-rpc.polkadot.io'),
-    [gebSignet.genesisHash]: wsProvider('wss://signet.geb.network/ws')
+    [westend.genesisHash]: wsProvider('wss://westend-rpc.polkadot.io'),
+    [paseo.genesisHash]: wsProvider('wss://paseo.rpc.amforc.com')
   }
 });
 
