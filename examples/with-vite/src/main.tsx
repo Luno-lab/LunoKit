647a79586d72842c23c5168bb09e6b844ca644bd
wbh1328551759 <1328551759@qq.com>
2025-06-10 11:22:50 +0800

    fix(with-vite): remove transports config

--- examples/with-vite/src/main.tsx
+++ examples/with-vite/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
-import {createConfig, kusama, LunoKitProvider, polkadot, polkadotjs, subwallet, wsProvider, westend, paseo} from '@luno-kit/ui'
+import { createConfig, kusama, LunoKitProvider, polkadot, polkadotjs, subwallet, wsProvider, westend, paseo } from '@luno-kit/ui'
 import '@luno-kit/ui/dist/styles.css'
 
 const connectors = [
@@ -14,14 +14,7 @@ const lunoConfig = createConfig({
   chains: [polkadot, kusama, westend, paseo],
   connectors: connectors,
   autoConnect: true,
-  transports: {
-    [polkadot.genesisHash]: wsProvider('wss://polkadot.api.onfinality.io/public-ws'),
-    [kusama.genesisHash]: wsProvider('wss://kusama-rpc.polkadot.io'),
-    [westend.genesisHash]: wsProvider('wss://westend-rpc.polkadot.io'),
-    [paseo.genesisHash]: wsProvider('wss://pas-rpc.stakeworld.io')
-  }
 });
-
 
 createRoot(document.getElementById('root')!).render(
   <LunoKitProvider config={lunoConfig}>
