f63ff083fd48070e63d8d6f78f39697334301ee6
WBH <48749219+wbh1328551759@users.noreply.github.com>
2025-07-23 17:40:02 +0800

    add example of create react app
    * chore: update pnpm-workspace.yaml to add dependency versions and use catalog as version source
    
    * chore(with-vite): update import paths
    
    * feat(with-nextjs-pages): add Next.js pages router example
    
    * feat(with-nextjs-app): add Next.js app router example
    
    * chore: update @types/react and @types/react-dom versions
    
    * feat(with-cra): add example using Create React App

---
+++ examples/with-cra/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ConnectButton } from '@luno-kit/ui'
+
+function App() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        padding: '12px',
+      }}
+    >
+      <ConnectButton/>
+    </div>
+  );
+}
+
+export default App;
