df92e26f3b2aec97a4606208fd2224144e5f5815
wbh1328551759 <1328551759@qq.com>
2025-07-23 16:46:21 +0800

    feat(with-nextjs-app): add Next.js app router example

--- examples/with-nextjs-app/app/page.tsx
+++ examples/with-nextjs-app/app/page.tsx
@@ -3,7 +3,7 @@ import { ConnectButton } from '@luno-kit/ui'
 export default function Home() {
   return (
     <div className={'flex justify-center p-4 w-full'}>
-      <ConnectButton />
+      <ConnectButton/>
     </div>
   );
 }
