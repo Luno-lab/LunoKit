62a60cda5b301b7096682cc45652e970ab1d8e15
wbh1328551759 <1328551759@qq.com>
2025-12-11 15:28:09 +0800

    feat(react): enhance useApi to support generic types

--- packages/react/src/hooks/useApi.ts
+++ packages/react/src/hooks/useApi.ts
@@ -1,17 +1,18 @@
 import type { LegacyClient } from 'dedot';
 import { useLuno } from './useLuno';
+import type { Optional } from '../types';
 
-export interface UseApiResult {
-  api?: LegacyClient;
+export interface UseApiResult<T extends LegacyClient = LegacyClient> {
+  api?: Optional<T>;
   isApiReady: boolean;
   apiError: Error | null;
 }
 
-export const useApi = (): UseApiResult => {
+export const useApi = <T extends LegacyClient = LegacyClient>(): UseApiResult<T> => {
   const { currentApi, isApiReady, apiError } = useLuno();
 
   return {
-    api: currentApi,
+    api: currentApi as T | undefined,
     isApiReady,
     apiError,
   };
